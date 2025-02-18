9e213055d349ab61b87e80aabee937db408fb65c
Mahad Kalam <mahadkalam1@proton.me>
2025-02-18 15:38:47 +0000

    Separate Design into its own component

--- src/lib/components/designer.svelte
+++ src/lib/components/designer.svelte
@@ -2,6 +2,7 @@
 import { appState } from "$lib/state.svelte";
 import ArrowLeft from "lucide-svelte/icons/arrow-left";
 import Button from "./button.svelte";
+import Design from "./design.svelte";
 </script>
 
 {#if appState.variant == "imageUploaded"}
@@ -14,23 +15,5 @@ import Button from "./button.svelte";
       Back
     </Button>
   </div>
-  <div class="w-full flex justify-center items-center">
-    <div
-      class="h-full flex items-center justify-center wallpaper{appState.wallpaperSelected}"
-      bind:this={appState.designerElement}
-      style="padding: {appState.padding}px;
-      border-radius: {appState.outerBorderRadius}px;
-      {typeof appState.wallpaperSelected === 'string' &&
-        `background-image: url('${appState.wallpaperSelected}')`}"
-    >
-      <div>
-        <img
-          src={URL.createObjectURL(appState.imageFile)}
-          alt="File uploaded"
-          style="border-radius: {appState.innerBorderRadius}px; border: {appState.inset}px solid {appState.insetColor};"
-          class="max-h-full"
-        />
-      </div>
-    </div>
-  </div>
+  <Design />
 {/if}
